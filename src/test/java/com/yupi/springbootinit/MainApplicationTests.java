f5628b62bc5c88f8a7a6953e349464a9373b27a3
vmuu <68872185@qq.com>
2024-12-01 14:50:33 +0800

    init

---
+++ src/test/java/com/yupi/springbootinit/MainApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yupi.springbootinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 主类测试
+ * @author XXXXX
+ */
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
